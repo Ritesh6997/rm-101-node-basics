a6374f2be3bff7b33e182d35809f502d1f87a9b5
Ritesh6997 <96717182+Ritesh6997@users.noreply.github.com>
2022-06-20 19:49:52 +0530

    upadate

--- src/server.js
+++ src/server.js
@@ -1,8 +1,22 @@
 // install and import express
-const express = () => {};
+const express = require("express");
+const path = require("path");
 let app = express();
+const port = 8000
 
 // Code here
 
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "../../src/assets/users.html"));
+});
+
+app.get("/users",(req, res) => {
+  res.sendFile(path.join(__dirname + "../../src/assets/user.json"));
+});
+
+app.listen(port, () => {
+})
 // Note: Do not remove this export statement
 module.exports = app;
